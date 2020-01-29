e24ab8706b35b28c550db1f3db3d0e409a2bb145
seokmin <essemfly@gmail.com>
2020-01-30 01:05:49 +0900

    Change dataframe append to list append

--- src/subdivide.py
+++ src/subdivide.py
@@ -1,28 +1,33 @@
 import pandas as pd
-from .utils import get_person_ids
+from .utils import get_person_ids, days_hours_minutes, get_birth_date
 from .constants import MEASUREMENT_SOURCE_VALUE_USES, measurement_csv, person_csv
+from datetime import datetime
 
 
-def divide(m_df, person_id):
+def divide(m_df, person_id, birth_date):
     m_df = m_df[m_df['PERSON_ID'] == person_id]
-    divided_df = pd.DataFrame()
+    m_df.loc[:, "MEASUREMENT_DATETIME"] = pd.to_datetime(m_df["MEASUREMENT_DATETIME"], format="%Y-%m-%d %H:%M")
+    m_df.sort_values("MEASUREMENT_DATETIME", inplace=True)
 
+    records = []
     new_personal_record = None
     for idx, row in m_df.iterrows():
         if new_personal_record is None:
-            new_personal_record = {"MEASUREMENT_DATETIME": row["MEASUREMENT_DATETIME"]}
+            new_personal_record = {"MEASUREMENT_DATETIME": row["MEASUREMENT_DATETIME"],
+                                   "TIME_FROM_BIRTH": days_hours_minutes(row["MEASUREMENT_DATETIME"] - birth_date)}
 
         elif new_personal_record["MEASUREMENT_DATETIME"] != row["MEASUREMENT_DATETIME"]:
-            divided_df = divided_df.append(new_personal_record, ignore_index=True)
-            new_personal_record = {"MEASUREMENT_DATETIME": row["MEASUREMENT_DATETIME"]}
+            records.append(new_personal_record)
+            new_personal_record = {"MEASUREMENT_DATETIME": row["MEASUREMENT_DATETIME"],
+                                   "TIME_FROM_BIRTH": days_hours_minutes(row["MEASUREMENT_DATETIME"] - birth_date)}
 
         if row["MEASUREMENT_SOURCE_VALUE"] in MEASUREMENT_SOURCE_VALUE_USES:
             new_personal_record[row["MEASUREMENT_SOURCE_VALUE"]] = row["VALUE_SOURCE_VALUE"]
 
     if new_personal_record is not None:
-        divided_df = divided_df.append(new_personal_record, ignore_index=True)
+        records.append(new_personal_record)
 
-    return divided_df
+    return pd.DataFrame(records)
 
 
 def subdivide(cfg, writer):
@@ -33,8 +38,9 @@ def subdivide(cfg, writer):
 
     for person_id in person_ids:
         print('Person: ', person_id)
+        birth_date = get_birth_date(p_df, person_id)
         try:
-            person_resampled_df = divide(m_df, person_id)
+            person_resampled_df = divide(m_df, person_id, birth_date)
             person_resampled_df.to_csv(f'{cfg.VOLUME_DIR}/{str(person_id)}.csv')
         except ValueError as e:
             print('e', e)
